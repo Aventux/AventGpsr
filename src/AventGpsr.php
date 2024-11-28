af89bbe015add1b6d564138545d55116078d91db
Tobias Poeschl <kontakt@aventux.com>
2024-11-28 08:45:22 +0100

    initial commit

---
+++ src/AventGpsr.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Avent\Gpsr;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\ActivateContext;
+use Shopware\Core\Framework\Plugin\Context\DeactivateContext;
+use Shopware\Core\Framework\Plugin\Context\InstallContext;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+use Shopware\Core\Framework\Plugin\Context\UpdateContext;
+
+class AventGpsr extends Plugin
+{}
